706bde881273fd11959de2b60dfc1ac27754886f
Makcuk <hui>
2016-12-23 01:21:38 +0300

    Курсач

---
+++ src/Kursovik/Main.java
@@ -0,0 +1,11 @@
+package Kursovik;
+
+public class Main {
+    public static void main(String[]args) throws Exception
+
+    {
+
+        new Panel();
+
+    }
+}
---
+++ src/Kursovik/WebConnector.java
@@ -0,0 +1,34 @@
+package Kursovik;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+
+ * Класс для веб запроса JSON объекта
+
+ */
+
+public class WebConnector {
+    public static String sendGet(String url) throws Exception {
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //returning result
+        return response.toString();
+
+    }
+}
